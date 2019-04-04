8413d338fd4cf3e85beba42640c6d4d8694e9814
Arthur Song <arth3rs0ng@gmail.com>
2019-04-04 01:17:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,5 +5,8 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length == 3
     "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    last = array.pop
+    "#{array.join(", ")}, and #{last}"
   end
 end
